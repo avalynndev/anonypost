e6d78163b7f28f7d39240b16bf54dbd2f333f941
Mplus <115032431+Mykel68@users.noreply.github.com>
2025-03-30 12:06:29 +0100

    Pwa feature (#133)
    * feat:next config
    
    * chore:added later generated files for pwa to gitignore
    
    * chore:manifest.json
    
    * chore:ignore migrations folder
    
    * chore:generate splash screens
    
    * chore:added meta tag for splash screens
    
    * feat: Add PWA functionality with splash screens and manifest
    
    * Delete package-lock.json
    
    ---------
    
    Co-authored-by: avalynn <100826194+avalynndev@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -2,9 +2,24 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
 
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const config = {};
 
-export default config;
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    disable: false,
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+});
+
+export default (async () => {
+    await import("./src/env.js");
+    return withPWA(config);
+})();
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,38 @@
 import "~/styles/globals.css";
-
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { TRPCReactProvider } from "~/trpc/react";
 import { Navbar } from "~/components/navbar";
 import { Footer } from "~/components/footer";
+import Meta from "~/components/meta";
 
 export const metadata: Metadata = {
+  manifest: "/manifest.json",
   title: "Anonymous Posts",
   description:
     "Share your thoughts and experiences anonymously by posting on our platform — no sign-up needed!",
+  openGraph: {
+    type: "website",
+    url: "https://anonypost.vercel.app",
+    title: "Anonymous Posts",
+    description:
+      "Share your thoughts and experiences anonymously by posting on our platform — no sign-up needed!",
+    images: [
+      {
+        url: "https://anonypost.vercel.app/splash/manifest-icon-192.maskable.png",
+        width: 192,
+        height: 192,
+        alt: "Anonymous Posts",
+      },
+      {
+        url: "https://anonypost.vercel.app/splash/manifest-icon-512.maskable.png",
+        width: 512,
+        height: 512,
+        alt: "Anonymous Posts",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <head>
+        <Meta />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
