907813706042d8b69c78ef9dcd6361027e8d43f4
avalynn <100826194+avalynndev@users.noreply.github.com>
2025-07-20 06:00:48 +0000

    added pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
-  /* config options here */
 };
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+export default (async () => {
+  return withPWA(nextConfig);
+})();
